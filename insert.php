e921a6d57315ad2740d5fd10801a4e7d11f09e85
Christopher Aring <ChrisssAring@gmail.com>
2018-06-25 11:10:50 -0400

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,6 +1,4 @@
 <?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "ssli-chrisaring", "SurrealLem0n!", "ssli_test");
  
 // Check connection
