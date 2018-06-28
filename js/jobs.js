24231fa3a8c59855e246d0a38dac2138a692f4a2
KyuBinKwon <kyubinkwon829@gmail.com>
2018-06-27 23:36:24 -0400

    jobs/education page

--- js/jobs.js
+++ js/jobs.js
@@ -1,23 +1,73 @@
 $(document).ready(function() {
-    console.log("I'M READY!!!");
 });
 
+// loop through data and append jobs to the div
+function addJobs() {
+    makeDummyData();
+    for (i = 0; i < dummyData.length; i++) {
+        var d = dummyData[i];
+        appendJob(d.id, d.pic_url, d.title, d.company, d.loc, d.desc, d.price_lo, d.price_hi, d.level, d.type);
+    }
+}
 
-let jobId = new Set();
+// function addEducation() {
+//     for (i = 0; i < dummyData.length; i++) {
+//         var d = dummyData[i];
+//         append(d.id, d.pic_url, d.title, d.company, d.loc, d.desc, d.price_lo, d.price_hi, d.level, d.type);
+//     }
+// }
 
+// list of dictionaries (job item); where the queried items will be
 let dummyData = []
-let data = {}
+function makeDummyData() {
+    dummyData.push({
+        id : 0,
+        pic_url : "../images/placeholder.png",
+        title : "Sustainability Research Analyst",
+        company : "Share Creators",
+        loc : "Seattle, WA",
+        desc : "Manages consultant scopes of work and agency stakeholder \
+            processes to implement specific sustainability goals and annual sustainability targets",
+        price_lo : 40000,
+        price_hi : 50000,
+        level : "Entry Level",
+        type : "Full time"
+    });
+}
+
 
 // function that makes a new job component and adds to the div
-function append(id, pic_url, username, at, tweet) {
-    // check for duplicates
-    if (jobId.has(id)) return;
-    else jobId.add(id);
+function appendJob(id, pic_url, title, company, loc, desc, price_lo, price_hi, level, type) {
+    // abridge the job description to the first 150 characters
+    var abridged = desc.substring(0, 150);
 
-    // append tweet
-    var ul = $("#tweets");
-    var li = $("<li class='tweet' id=" + String(id) + "></li>");
-    li.html("<img class='pp' src=" + pic_url + "></img><b>" + username +
-        "</b> <span class='at'> @" + at + "</span><br>" + tweet);
-    ul.prepend(li);
+    // append job
+    var div = $("#results-container");
+
+    var subDiv = $("<div class='jobItem shadow list-inline' id=" + String(id) + "> \
+        <img class='list-inline-item jobIcon align-top' src=" + pic_url + "alt='Icon'> \
+        <div class='list-inline-item jobDiv'> <h4 class='gray-text bold'>" + title +
+        "</h4><p class='gray-text'>" + company + "&nbsp; <span class='teal-text'>" + loc +
+        "</span></p><p class='gray-text Xsmall jobDesc'>" + abridged +
+        "<span class='blue-text'> more... </span></p><p class='teal-text Xsmall'>$" + String(price_lo) +
+        " - $" + String(price_hi) + " • " + level + " • " + type + "</p></div><button type='button' \
+        onclick='toggle.call(this)' class='btn btn-outline-success list-inline-item checkbox align-top'>✓</button></div>")
+
+    div.append(subDiv);
+}
+
+
+
+
+let selectedJobs = new Set();
+var toggle = function() {
+    // var id = // get id of job
+    var clicked = this.style.background == "rgb(144, 178, 183)";    // if it was previously clicked
+    if (clicked) {      // make it clear, remove from set
+        // selectedJobs.delete(id);
+        this.style.background = 'transparent';
+    } else {            // color it, add to set
+        // selectedJobs.add(id);
+        this.style.background = "rgb(144, 178, 183)";
+    }
 }
