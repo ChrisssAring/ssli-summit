51ff8217c8c31bbe2b07d5c0daaf3746af2e9a7e
KyuBinKwon <kyubinkwon829@gmail.com>
2018-06-28 20:30:03 -0400

    added for education.html too

--- js/jobs.js
+++ js/jobs.js
@@ -1,49 +1,28 @@
-$(document).ready(function() {
-    // addJobs();
-});
-
-var total = 250;                            // TODO change this value later to reflect size of database
-
 ///////////////////////////////////////////////
 // loop through data and append jobs to the div
 ///////////////////////////////////////////////
 function addJobs() {
-    // makeDummyData();
-
     // if there is more jobs than 20, limit the number
-    var num = Math.min(dummyData.length, 20);
+    var num = Math.min(dummyJobData.length, 20);
 
-    $("#pageNav").text("1-" + num + " of " + total);
+    $("#pageNav").text("1-" + num + " of " + dummyJobData.length);
 
-    for (i = 0; i < dummyData.length; i++) {
-        var d = dummyData[i];
+    for (i = 0; i < dummyJobData.length; i++) {
+        var d = dummyJobData[i];
         appendJob(d.id, d.pic_url, d.title, d.company, d.loc, d.desc, d.price_lo, d.price_hi, d.level, d.type);
     }
 }
 
-// function addEducation() {
-//     for (i = 0; i < dummyData.length; i++) {
-//         var d = dummyData[i];
-//         append(d.id, d.pic_url, d.title, d.company, d.loc, d.desc, d.price_lo, d.price_hi, d.level, d.type);
-//     }
-// }
+function addEducation() {
+    // if there is more jobs than 20, limit the number
+    var num = Math.min(dummyEduData.length, 20);
 
-// list of dictionaries (job item); where the queried items will be
-let dummyData = []
-function makeDummyData() {
-    dummyData.push({
-        id : 0,
-        pic_url : "../images/placeholder.png",
-        title : "Sustainability Research Analyst",
-        company : "Share Creators",
-        loc : "Seattle, WA",
-        desc : "Manages consultant scopes of work and agency stakeholder \
-            processes to implement specific sustainability goals and annual sustainability targets",
-        price_lo : 40000,
-        price_hi : 50000,
-        level : "Entry Level",
-        type : "Full time"
-    });
+    $("#pageNav").text("1-" + num + " of " + dummyEduData.length);
+
+    for (i = 0; i < dummyEduData.length; i++) {
+        var d = dummyEduData[i];
+        appendEdu(d.id, d.pic_url, d.title, d.company, d.loc, d.desc, d.tuition, d.level);
+    }
 }
 
 ///////////////////////////////////////////////
@@ -57,12 +36,32 @@ function appendJob(id, pic_url, title, company, loc, desc, price_lo, price_hi, l
 
     // make new html component
     var subDiv = $("<div class='jobItem shadow list-inline' id=" + String(id) + "> \
-        <img class='list-inline-item jobIcon align-top' src=" + pic_url + "alt='Icon'> \
+        <img class='list-inline-item jobIcon align-top' src=" + pic_url + " alt='Icon'> \
         <div class='list-inline-item jobDiv'> <h4 class='gray-text bold'>" + title +
         "</h4><p class='gray-text'>" + company + "&nbsp; <span class='teal-text'>" + loc +
         "</span></p><p class='gray-text Xsmall jobDesc'>" + abridged +
         "<span class='blue-text'> more... </span></p><p class='teal-text Xsmall'>$" + String(price_lo) +
         " - $" + String(price_hi) + " • " + level + " • " + type + "</p></div><button type='button' \
+        onclick='toggle.call(this)' class='btn btn-outline-success list-inline-item checkbox align-top'>✓</button></div>")
+
+    // append to existing div
+    var div = $("#results-container");
+    div.append(subDiv);
+}
+
+function appendEdu(id, pic_url, title, company, loc, desc, tuition, level) {
+    // abridge the job description to the first 150 characters
+    var abridged = desc.substring(0, 150);
+
+    var checkboxID = "C" + String(id); // unique ID for the checkbox; used later to get the job ID
+
+    // make new html component
+    var subDiv = $("<div class='jobItem shadow list-inline' id=" + String(id) + "> \
+        <img class='list-inline-item jobIcon align-top' src=" + pic_url + " alt='Icon'> \
+        <div class='list-inline-item jobDiv'> <h4 class='gray-text bold'>" + title +
+        "</h4><p class='gray-text'>" + company + "&nbsp; <span class='teal-text'>" + loc +
+        "</span></p><p class='gray-text Xsmall jobDesc'>" + abridged +
+        "<span class='blue-text'> more... </span></p><p class='teal-text Xsmall'>$" + tuition + " • " + level + "</p></div><button type='button' \
         onclick='toggle.call(this)' class='btn btn-outline-success list-inline-item checkbox align-top'>✓</button></div>")
 
     // append to existing div
@@ -108,6 +107,8 @@ function folderOnClick() {
         $("#email-container").css("position", "absolute");
         $("#email-container").css("height", "0px");
     }
+
+    console.log(selectedJobs.size);
 }
 
 /////////////////////////////////////////////////////////
@@ -118,8 +119,95 @@ function showEmailForm() {
 }
 
 function sendEmail() {
+    // hide popup
     $("#popup-container").css("visibility", "hidden");
+
+    // hide opportunities div
     $("#email-container").css("visibility", "hidden");
     $("#email-container").css("position", "absolute");
     $("#email-container").css("height", "0px");
+}
+
+
+
+
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////// TODO delete this when database comes along
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+// list of dictionaries (job item); where the queried items will be
+let dummyJobData = []
+let dummyEduData = []
+function makeDummyData() {
+    dummyJobData.push({
+        id : 0,
+        pic_url : "../images/placeholder.png",
+        title : "Sustainability Research Analyst",
+        company : "Share Creators",
+        loc : "Seattle, WA",
+        desc : "Manages consultant scopes of work and agency stakeholder \
+            processes to implement specific sustainability goals and annual sustainability targets",
+        price_lo : 40000,
+        price_hi : 50000,
+        level : "Entry Level",
+        type : "Full time"
+    });
+
+    dummyEduData.push( {
+        id : 1,
+        pic_url : "../images/placeholder.png",
+        title : "Organic Gardening",
+        company : "Emory University",
+        loc : "Atlanta, GA",
+        desc : "From a conservation to global health to energy markets to \
+            social innovation, Emory has a student organization for every sustainable interest",
+        tuition : "5000/semester",
+        level : "Associate, Bachelor"
+    });
+
+    dummyJobData.push({
+        id : 2,
+        pic_url : "../images/placeholder.png",
+        title : "Principal SDE, Sustainability",
+        company : "Amazon",
+        loc : "Seattle, WA",
+        desc : "The Seattle Housing Authority, a nationally recognized leader blahblah",
+        price_lo : 60000,
+        price_hi : 55000,
+        level : "Mid Level",
+        type : "Part time"
+    });
+
+    dummyJobData.push({
+        id : 3,
+        pic_url : "../images/placeholder.png",
+        title : "Business Intelligence Engineer - Sustainability",
+        company : "Share Creators",
+        loc : "Seattle, WA",
+        desc : "At Hinge Health, our mission is to improve the lives of \
+                people suffering from chronic conditions by digitizing the \
+                delivery of care - starting with",
+        price_lo : 70000,
+        price_hi : 75000,
+        level : "Entry Level",
+        type : "Contract"
+    });
+
+    dummyJobData.push({
+        id : 4,
+        pic_url : "../images/placeholder.png",
+        title : "Climate and Sustainability Writer and Analyst",
+        company : "Amazon",
+        loc : "Seattle, WA",
+        desc : "Manages consultant scopes of work and agency stakeholder \
+            processes to implement specific sustainability goals and annual sustainability targets",
+        price_lo : 80000,
+        price_hi : 85000,
+        level : "Senior Level",
+        type : "Full time"
+    });
 }
